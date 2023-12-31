b92fae18096d8eb816ed9a45de4fa86878fe9129
SSH <12si47bun@gmail.com>
2023-12-31 20:37:01 +0900

    Route로 나누기 전 DataSort컴포 Rank컴포로 변경

---
+++ src/Component/Rank.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import card from '../Style/card.module.scss';
+import { getCardColor, getDustState, getEmojiState } from '../Data/DustStatus';
+import { Bookmark } from './Bookmark';
+import { AllDataGet } from '../Data/AllDataGet';
+
+const Rank = () => {
+  const [sortNum, setSortNum] = useState(10); // n개씩 보기
+  const [sortDirection, setSortDirection] = useState('tobad'); // 오름차 내림차
+
+  const handleSortNumChange = e => {
+    setSortNum(Number(e.target.value));
+  };
+
+  const handleSortDirectionChange = e => {
+    setSortDirection(e.target.value);
+  };
+
+
+  // AllDataGet : 모든 데이터 한번에 가져오기
+  const alldata = AllDataGet();
+  let sortedData;
+
+  if (sortDirection === 'tobad') {
+    sortedData = alldata
+      .filter(el => el.pm10Value !== '-' && el.pm10Value !== null) // 통신오류로 미세먼지값이 -, null 경우 배제하기
+      .sort((a, b) => parseFloat(b.pm10Value) - parseFloat(a.pm10Value))
+      .slice(0, sortNum);
+  } else if (sortDirection === 'togood') {
+    sortedData = alldata
+      .filter(el => el.pm10Value !== '-' && el.pm10Value !== null) // 통신오류로 미세먼지값이 -, null 경우 배제하기
+      .sort((a, b) => parseFloat(a.pm10Value) - parseFloat(b.pm10Value))
+      .slice(0, sortNum);
+  } else {
+    console.log('sortDirection 입력오류');
+  }
+
+  return (
+    <section>
+      <h3>검색 및 순위보기</h3>
+      <button onClick={()=>{setSortNum(5)}}>TOP5</button>
+      <button onClick={()=>{setSortNum(10)}}>TOP10</button>
+
+      <label>정렬 개수:</label>
+      <input type="number" value={sortNum} onChange={handleSortNumChange} />
+
+      <label>정렬 방향:</label>
+      <select value={sortDirection} onChange={handleSortDirectionChange}>
+        <option value="tobad">미세먼지 안 좋은 곳부터 보기</option>
+        <option value="togood">미세먼지 좋은 곳부터 보기</option>
+      </select>
+
+      <div className={card.cardOuter}>
+      {sortedData && sortedData.map((el, idx) => (
+        <div
+          key={el.sidoName + el.stationName}
+          className={card.cardContainer}
+          style={{
+            backgroundColor: getCardColor(el.pm10Value),
+          }}
+        >
+          <div className={card.rank}>{idx + 1}위</div>
+          <div className={card.cardWrapTop}>
+            <div className={card.sidoName}>{el.sidoName}</div>
+            <Bookmark sidoName={el.sidoName} stationName={el.stationName} pm10Value={el.pm10Value} dataTime={el.dataTime} />
+            <div className={card.stationName}>{el.stationName}</div>
+          </div>
+          <div className={card.cardWrapMiddle}>
+            <div className={card.emoji}>{getEmojiState(el.pm10Value)}</div>
+            <div className={card.dustState}>{getDustState(el.pm10Value)}</div>
+          </div>
+          <div className={card.dustValue}>
+            미세먼지 : {el.pm10Value}
+          </div>
+          <div className={card.dataTime}>{el.dataTime} 기준</div>
+        </div>
+      ))}
+    </div>
+    </section>
+  );
+};
+
+export default Rank;
+
+
+
+
--- src/King.js
+++ src/King.js
@@ -1,37 +1,17 @@
 import king from './Style/king.module.scss';
 import React, { useState } from 'react';
 import { DustCard } from './Component/DustCard';
-import { DataSort } from './Component/DataSort';
+import  Rank from './Component/Rank';
 import BookmarksList from './Component/BookmarkList';
-import SortControl from './Component/SortControl';
 
 export default function King() {
-  const [sortNum, setSortNum] = useState(5);
-  const [sortDirection, setSortDirection] = useState("tobad");
-  const [searchData, setSearchData] = useState("정자");
-
-  const handleSortNumChange = (e) => {
-    setSortNum(parseInt(e.target.value, 10));
-  };
-
-  const handleSortDirectionChange = (e) => {
-    setSortDirection(e.target.value);
-  };
 
   return (
     <section className={king.common}>
       <h3>기본</h3>
       <DustCard selectedSido="서울" numOfRows={3} />
-      <h3>검색 및 순위보기</h3>
-      <SortControl
-        sortNum={sortNum}
-        sortDirection={sortDirection}
-        searchData={searchData}
-        onSortNumChange={handleSortNumChange}
-        onSortDirectionChange={handleSortDirectionChange}
-        onSearchDataChange={setSearchData}
-      />
-      <DataSort sortNum={sortNum} sortDirection={sortDirection} />
+      
+      <Rank />
       <h3>북마크리스트</h3>
       <BookmarksList />
     </section>
